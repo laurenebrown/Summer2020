f6469c7f97bf439ec2a1e43cb4ffbece98d42ddf
lauren2 <brownl23@greenhill.org>
2020-06-26 09:41:54 -0600

    Software Update

---
+++ src/com/company/gangstanamenew.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.*;
+
+public class gangstanamenew {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Please enter your full name");
+        String player = sc.nextLine();
+        int indexOf= player.indexOf(" ");
+        String firstName = player.substring(0,indexOf);
+        String lastName = player.substring(indexOf+1);
+        lastName= lastName.toUpperCase();
+        char l= firstName.charAt(0);
+
+        System.out.println(l + ". Diddy " + lastName +" " + firstName+ "-izzle");
+    }
+}
+
