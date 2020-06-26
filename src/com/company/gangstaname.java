9bde2cf0c2944594eb67ee026b77d09c0cfc05dc
lauren2 <brownl23@greenhill.org>
2020-06-26 09:42:38 -0600

    Software Update

--- src/com/company/gangstaname.java
+++ src/com/company/gangstaname.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.*;
 
-public class gangstanamenew {
+public class gangstaname {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
